0eff28ab0997ea2bd3984913fb57170f04b396a6
Alejandro Ascencio Anaya <71231638+po171@users.noreply.github.com>
2023-10-03 22:15:35 -0600

    Initial Push

---
+++ action.php
@@ -0,0 +1,10 @@
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$subject = $_POST['subject'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "ascencioanayaalejandro@gmail.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
